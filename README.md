7ffb7c85475a4a0453ff997e154c1e8e1f17ae55
Nicolas Crombois <nicolas.crombois@hotmail.fr>
2021-04-09 11:59:50 +0200

    Add function displayArticle + Edit README.md

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
+#MVP ORINOCO#
+
+###Démarrer l'API & Modifier l'adresse
+
 Afin de visualiser le projet correctement, l'api doit être lancé.
 Pour cela, NodeJs doit être installé sur la machine.
 
 Au préalable cloner le repository Git sur votre machine.
 Une fois cela fait, exécuter votre Terminal puis faites : 
-->   cd path_du_projet/api/
-->   npm install
-->   node server
+1. *cd path_du_projet/api/*
+2. *npm install*
+3. *node server*
 
 Maintenant votre terminal devrait vous afficher le message de réussite suivant:
-" Successfully connected to MongoDB Atlas! "
+*" Successfully connected to MongoDB Atlas! "*
 Dans ce cas, l'api a bien été lancé.
+
 Le port d'écoute de l'api est renseigné sur le terminal précédant le message de réussite :
-"Listening on port 3000"
-Par défaut, votre api est utilisable sur l'adresse : http://localhost:3000/
+*" Listening on port 3000 "*
+Par défaut, votre api est utilisable sur l'adresse : **http://localhost:3000/**
 
 Si votre adresse ne ressemble pas à celle-ci, veuillez modifier le fichier src/js/host.js et y mettre votre adresse.
--- src/js/product.js
+++ src/js/product.js
@@ -98,7 +98,108 @@ function addBasket(element) {
         displayAdditionMessage(specificity, quantity) }
     numberArticleBasket()
 }
+function displayArticle(element){
+    // Récupérer l'élément dans lequel les informations poussé
+    const productDiv = document.getElementById('product');
 
+    // Créer l'ensemble des éléments HTML nécessaire à la mise en page
+    var h1 = document.createElement('h1');
+    var img = document.createElement('img');
+    var divImage = document.createElement('div');
+    var productGlobalForm = document.createElement('div');
+    var customSentence = document.createElement('p');
+    var button = document.createElement('button');
+    var containerPrice = document.createElement('p');
+    var labelQuantity = document.createElement('label');
+    var inputQuantity = document.createElement('input');
+    var span = document.createElement('span');
+    var divGlobalCustom = document.createElement('div');
+
+    // Ajoût des classes à ces éléments
+    h1.classList.add("product-name");
+    h1.classList.add("text-center");
+    img.classList.add("product-img");
+    divImage.classList.add("product-divImage");
+    productGlobalForm.classList.add("productGlobalForm");
+    customSentence.classList.add("customText");
+    button.classList.add("btn");
+    button.classList.add("btn-primary");
+    containerPrice.classList.add("product-ContainerPrice");
+    labelQuantity.classList.add("customText");
+    inputQuantity.classList.add("product-input-quantity");
+    span.classList.add("product-price");
+    divGlobalCustom.classList.add("product-globalCustom");
+
+    // Ajoût de la balise alt spécifique 
+    if (category == "cameras") {
+        img.alt = "Photo de la caméra " + element.name;
+    } else if (category == "teddies") {
+        img.alt = "Photo de la peluche " + element.name;
+    } else if (category == "furniture") {
+        img.alt = "Photo d'un " + element.name;
+    }
+    // Ajoût de la source de l'image
+    img.src = element.imageUrl;
+
+    // Ajoût des textes dans les éléments nécessaires
+    h1.innerHTML = element.name;
+    inputQuantity.value = "1";
+    customSentence.innerHTML = "Choississez la personnalisation qui vous convient :";
+    button.innerHTML = "Ajouter au panier";
+    span.innerHTML = (element.price / 100).toFixed(2) + "€";
+    containerPrice.innerHTML = "Prix : ";
+    labelQuantity.innerHTML = "Quantité désirée :";
+
+    // Ajoût des paramètres et du texte nécessaire sur l'élément input et label
+    labelQuantity.htmlFor = "quantity";
+    inputQuantity.type = "number";
+    inputQuantity.name = "quantity"
+    inputQuantity.min = 1;
+    inputQuantity.max = 99;
+
+    // Ajoût d'une écoute sur la quantité entrée afin de modifier le prix en fonction 
+    inputQuantity.addEventListener("keyup", calculatePrice, false);
+    inputQuantity.addEventListener("mouseup", calculatePrice, false);
+
+    // Ajoût d'une écoute sur le bouton "ajouter au panier" afin d'exécuter la fonction d'ajouter au basket 
+    button.addEventListener("click", function () { addBasket(element) }, false);
+
+    // Ensemble des éléments sont enfin ajoutés dans l'élément parent
+    productDiv.appendChild(h1);
+    divImage.appendChild(img);
+    productDiv.appendChild(divImage);
+    divGlobalCustom.appendChild(customSentence);
+    // Les options de personnalisation doivent être formaté/ajouté au compte-gouttes
+    element[customByCategory[category]].forEach(customElement => {
+        // Créer les balises input et label et de la balise parent (div); Attribution des classes
+        var divForm = document.createElement('div');
+        var input = document.createElement('input');
+        var label = document.createElement('label');
+        divForm.classList.add("product-form");
+        divForm.classList.add("form-check");
+        input.classList.add("form-check-input");
+        label.classList.add("form-check-label");
+        // Ajoût des paramètres sur l'élément input
+        input.type = "radio";
+        input.name = "custom";
+        // Ajoût des paramètres et du texte nécessaire sur les éléments input et label
+        input.id = customElement;
+        label.htmlFor = customElement;
+        label.innerHTML = customElement;
+        // Ajoût des input et label dans la div parent
+        divForm.appendChild(input);
+        divForm.appendChild(label);
+        // Ajout des div contenant les label et input dans une div contenant l'ensemble des options
+        productGlobalForm.appendChild(divForm);
+    });
+    divGlobalCustom.appendChild(productGlobalForm);
+    divGlobalCustom.appendChild(labelQuantity);
+    divGlobalCustom.appendChild(inputQuantity);
+    productDiv.appendChild(divGlobalCustom);
+    containerPrice.appendChild(span);
+    productDiv.appendChild(containerPrice);
+    productDiv.appendChild(button);
+}
 numberArticleBasket()
 // Créer un tableau regroupant les adresses API (les adresses qui récupérent l'ensemble des tableaux des produits de la catégorie)
 let allItemsUrl = { "cameras": host + "api/cameras/", "teddies": host + "api/teddies/", "furniture": host + "api/furniture/" };
@@ -111,106 +212,7 @@ request.onreadystatechange = function () {
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
         var element = JSON.parse(this.responseText);
 
-        // Récupérer l'élément dans lequel les informations poussé
-        const productDiv = document.getElementById('product');
-
-        // Créer l'ensemble des éléments HTML nécessaire à la mise en page
-        var h1 = document.createElement('h1');
-        var img = document.createElement('img');
-        var divImage = document.createElement('div');
-        var productGlobalForm = document.createElement('div');
-        var customSentence = document.createElement('p');
-        var button = document.createElement('button');
-        var containerPrice = document.createElement('p');
-        var labelQuantity = document.createElement('label');
-        var inputQuantity = document.createElement('input');
-        var span = document.createElement('span');
-        var divGlobalCustom = document.createElement('div');
-
-        // Ajoût des classes à ces éléments
-        h1.classList.add("product-name");
-        h1.classList.add("text-center");
-        img.classList.add("product-img");
-        divImage.classList.add("product-divImage");
-        productGlobalForm.classList.add("productGlobalForm");
-        customSentence.classList.add("customText");
-        button.classList.add("btn");
-        button.classList.add("btn-primary");
-        containerPrice.classList.add("product-ContainerPrice");
-        labelQuantity.classList.add("customText");
-        inputQuantity.classList.add("product-input-quantity");
-        span.classList.add("product-price");
-        divGlobalCustom.classList.add("product-globalCustom");
-
-        // Ajoût de la balise alt spécifique 
-        if (category == "cameras") {
-            img.alt = "Photo de la caméra " + element.name;
-        } else if (category == "teddies") {
-            img.alt = "Photo de la peluche " + element.name;
-        } else if (category == "furniture") {
-            img.alt = "Photo d'un " + element.name;
-        }
-        // Ajoût de la source de l'image
-        img.src = element.imageUrl;
-
-        // Ajoût des textes dans les éléments nécessaires
-        h1.innerHTML = element.name;
-        inputQuantity.value = "1";
-        customSentence.innerHTML = "Choississez la personnalisation qui vous convient :";
-        button.innerHTML = "Ajouter au panier";
-        span.innerHTML = (element.price / 100).toFixed(2) + "€";
-        containerPrice.innerHTML = "Prix : ";
-        labelQuantity.innerHTML = "Quantité désirée :";
-
-        // Ajoût des paramètres et du texte nécessaire sur l'élément input et label
-        labelQuantity.htmlFor = "quantity";
-        inputQuantity.type = "number";
-        inputQuantity.name = "quantity"
-        inputQuantity.min = 1;
-        inputQuantity.max = 99;
-
-        // Ajoût d'une écoute sur la quantité entrée afin de modifier le prix en fonction 
-        inputQuantity.addEventListener("keyup", calculatePrice, false);
-        inputQuantity.addEventListener("mouseup", calculatePrice, false);
-
-        // Ajoût d'une écoute sur le bouton "ajouter au panier" afin d'exécuter la fonction d'ajouter au basket 
-        button.addEventListener("click", function () { addBasket(element) }, false);
-
-        // Ensemble des éléments sont enfin ajoutés dans l'élément parent
-        productDiv.appendChild(h1);
-        divImage.appendChild(img);
-        productDiv.appendChild(divImage);
-        divGlobalCustom.appendChild(customSentence);
-        // Les options de personnalisation doivent être formaté/ajouté au compte-gouttes
-        element[customByCategory[category]].forEach(customElement => {
-            // Créer les balises input et label et de la balise parent (div); Attribution des classes
-            var divForm = document.createElement('div');
-            var input = document.createElement('input');
-            var label = document.createElement('label');
-            divForm.classList.add("product-form");
-            divForm.classList.add("form-check");
-            input.classList.add("form-check-input");
-            label.classList.add("form-check-label");
-            // Ajoût des paramètres sur l'élément input
-            input.type = "radio";
-            input.name = "custom";
-            // Ajoût des paramètres et du texte nécessaire sur les éléments input et label
-            input.id = customElement;
-            label.htmlFor = customElement;
-            label.innerHTML = customElement;
-            // Ajoût des input et label dans la div parent
-            divForm.appendChild(input);
-            divForm.appendChild(label);
-            // Ajout des div contenant les label et input dans une div contenant l'ensemble des options
-            productGlobalForm.appendChild(divForm);
-        });
-        divGlobalCustom.appendChild(productGlobalForm);
-        divGlobalCustom.appendChild(labelQuantity);
-        divGlobalCustom.appendChild(inputQuantity);
-        productDiv.appendChild(divGlobalCustom);
-        containerPrice.appendChild(span);
-        productDiv.appendChild(containerPrice);
-        productDiv.appendChild(button);
+        displayArticle(element)
 
     }
 }
