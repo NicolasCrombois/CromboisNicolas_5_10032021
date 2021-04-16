c955931372f6af7d7b066608fd3be0a1ba055145
Nicolas Crombois <nicolas.crombois@hotmail.fr>
2021-04-16 12:32:18 +0200

    modification of the postOrder and viewProduct function

--- src/js/default.js
+++ src/js/default.js
@@ -1,58 +1,62 @@
+function displayProduct(elementList){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            response.forEach(element => {
+                let span = document.createElement('span');
+                let h3 = document.createElement('h3');
+                let p = document.createElement('p');
+                let divBody = document.createElement('div');
+                let a = document.createElement('a');
+                let divContainer = document.createElement('div');
+                let img = document.createElement('img');
+
+                p.classList.add("card-text");
+                h3.classList.add("card-title");
+                divBody.classList.add("card-body");
+                divContainer.classList.add("card");
+                divContainer.id = element._id;
+                img.classList.add("card-img-top");
+                
+                if (elementList == host + "api/cameras/") {
+                    img.alt = "Photo de la caméra " + element.name;
+                } else if (elementList == host + "api/teddies/") {
+                    img.alt = "Photo de la peluche " + element.name;
+                } else if (elementList == host + "api/furniture/") {
+                    img.alt = "Photo d'un " + element.name;
+                }
+                img.src = element.imageUrl;
+                span.innerHTML = (element.price / 100).toFixed(2) + "€";
+                p.innerHTML = element.description;
+                h3.innerHTML = element.name;
+                if (elementList == host + "api/cameras/") {
+                    a.href = "product.html?category=cameras&id=" + element._id;
+                } else if (elementList == host + "api/teddies/") {
+                    a.href = "product.html?category=teddies&id=" + element._id;
+                } else if (elementList == host + "api/furniture/") {
+                    a.href = "product.html?category=furniture&id=" + element._id;
+                }
+                divBody.appendChild(img);
+                divBody.appendChild(h3);
+                divBody.appendChild(p);
+                divBody.appendChild(span);
+                a.appendChild(divBody);
+                divContainer.appendChild(a);
+
+                const cardList = document.querySelector('#main .card-list')
+                cardList.appendChild(divContainer);
+            });
+        }
+    };
+    request.open("GET", elementList);
+    request.send();
+}
+
 //fonction permettant d'afficher l'ensemble des produits
-function displayProduct(listApi) {
+function displayAllProduct(listApi) {
     listApi.forEach(elementList => {
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function () {
-            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-                var response = JSON.parse(this.responseText);
-                response.forEach(element => {
-                    let span = document.createElement('span');
-                    let h3 = document.createElement('h3');
-                    let p = document.createElement('p');
-                    let divBody = document.createElement('div');
-                    let a = document.createElement('a');
-                    let divContainer = document.createElement('div');
-                    let img = document.createElement('img');
-
-                    p.classList.add("card-text");
-                    h3.classList.add("card-title");
-                    divBody.classList.add("card-body");
-                    divContainer.classList.add("card");
-                    divContainer.id = element._id;
-                    img.classList.add("card-img-top");
-                    
-                    if (elementList == host + "api/cameras/") {
-                        img.alt = "Photo de la caméra " + element.name;
-                    } else if (elementList == host + "api/teddies/") {
-                        img.alt = "Photo de la peluche " + element.name;
-                    } else if (elementList == host + "api/furniture/") {
-                        img.alt = "Photo d'un " + element.name;
-                    }
-                    img.src = element.imageUrl;
-                    span.innerHTML = (element.price / 100).toFixed(2) + "€";
-                    p.innerHTML = element.description;
-                    h3.innerHTML = element.name;
-                    if (elementList == host + "api/cameras/") {
-                        a.href = "product.html?category=cameras&id=" + element._id;
-                    } else if (elementList == host + "api/teddies/") {
-                        a.href = "product.html?category=teddies&id=" + element._id;
-                    } else if (elementList == host + "api/furniture/") {
-                        a.href = "product.html?category=furniture&id=" + element._id;
-                    }
-                    divBody.appendChild(img);
-                    divBody.appendChild(h3);
-                    divBody.appendChild(p);
-                    divBody.appendChild(span);
-                    a.appendChild(divBody);
-                    divContainer.appendChild(a);
-
-                    const cardList = document.querySelector('#main .card-list')
-                    cardList.appendChild(divContainer);
-                });
-            }
-        };
-        request.open("GET", elementList);
-        request.send();
+        displayProduct(elementList)
     });
 }
 
@@ -71,9 +75,9 @@ function selectCategoryProduct() {
     document.querySelectorAll('div.card').forEach(element => {
         element.remove()
     })
-    displayProduct(arrayApiUrlCategorySelected)
+    displayAllProduct(arrayApiUrlCategorySelected)
 }
 
 numberArticleBasket()
 let allItemsUrl = [host + "api/cameras/", host + "api/teddies/", host + "api/furniture/"];
-displayProduct(allItemsUrl)
+displayAllProduct(allItemsUrl)
